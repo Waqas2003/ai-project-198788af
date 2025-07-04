72f62a6e7fd70cdf147dc01d77444046f4501527
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-05 01:38:50 +0500

    Add src\components\Footer.js

---
+++ src\components\Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer mt-auto py-3 bg-light">
+      <div className="container">
+        <p>&copy; 2023 Hollywood Website</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
